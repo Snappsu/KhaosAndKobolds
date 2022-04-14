1939de870676610a3566f86fbed0a1dce790ac08
Snapps <snapples64@gmail.com>
2022-04-14 14:12:49 -0700

    Implimented a loading method

---
+++ KnKTestSys/Character.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+class Character
+{
+    public Character()
+    {
+        this.name = "Wow";
+    }
+
+    private string name;
+    public string getName()
+    {
+        return name;
+    }
+    public void setName(string name)
+    {
+        this.name = name;
+    }
+
+    private Species species = new Species( new string[2] { "null", "null" } );
+    public Species getSpecies() {
+        return species;
+    }
+    public void setSpecies(string name, List<Species> speciesList)
+    {
+        foreach (Species spec in speciesList)
+        {
+            if (name == spec.getId()) species = spec;
+
+        }
+    }
+}
+
+class Species
+{
+    public static List<Species> loadSpecies()
+    {
+        List<Species> species = new List<Species>();
+        string[] lines = System.IO.File.ReadAllLines(Directory.GetCurrentDirectory() + "\\data\\species.txt");
+        foreach (string line in lines)
+        {
+            String[] speciesData = line.Split(',');
+            Species tempSpecies = new Species(speciesData);
+            Console.WriteLine(tempSpecies.stringSummary());
+            species.Add(tempSpecies);
+        }
+        return species;
+    }
+
+    public Species(string[] data)
+    {
+        id = data[0];
+        name = data[1];
+    }
+
+    private string id;
+    public string getId()
+    {
+        return id;
+    }
+
+    private string name;
+    public string getName()
+    {
+        return name;
+    }
+
+
+
+
+    string stringSummary() {
+        return id + ":" + name;
+    }
+}
--- KnKTestSys/Program.cs
+++ KnKTestSys/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            List<Species> allSpecies = Species.loadSpecies();
+            Character test = new Character();
+            Console.WriteLine(test.getSpecies().getName());
+            test.setSpecies("base2", allSpecies);
+            Console.WriteLine(test.getSpecies().getName());
+
+        }
+    }
+}
